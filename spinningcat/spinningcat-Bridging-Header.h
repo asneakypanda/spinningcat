eb7ff2bf310640dc0ebb028bda38685a3c66409e
Ethan <etarrer@gmail.com>
2024-11-12 17:13:57 -0800

    Initial Commit

---
+++ spinningcat/spinningcat-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  spinningcat-Bridging-Header.h
+//  spinningcat
+//
+//  Created by Ethan Tarrer on 11/12/24.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
---
+++ spinningcatTests/spinningcatTests.swift
@@ -0,0 +1,17 @@
+//
+//  spinningcatTests.swift
+//  spinningcatTests
+//
+//  Created by Ethan Tarrer on 11/12/24.
+//
+
+import Testing
+@testable import spinningcat
+
+struct spinningcatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
